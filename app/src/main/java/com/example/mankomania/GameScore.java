b9470c817d302d2aba335d22b65f4aeb6dd4174e
laulaobm <131969052+laulaobm@users.noreply.github.com>
2024-03-28 19:05:24 +0100

    Activities (Login, GameScore, ChooseYourCharacter) erstellt
    Die drei Activities wurden angelegt und mithilfe von Intents jeweils über einen Button verbunden. Design ist vorläufig. Strings und Colors wurden in den dazugehörigen xml Files abgelegt.

---
+++ app/src/main/java/com/example/mankomania/GameScore.java
@@ -0,0 +1,36 @@
+package com.example.mankomania;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class GameScore extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_game_score);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button resumeGame=findViewById(R.id.GameScore_ResumeGame);
+        resumeGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent resumeGameIntent=new Intent(GameScore.this, ChooseYourCharacter.class);
+                startActivity(resumeGameIntent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/mankomania/MainActivityLogin.java
@@ -0,0 +1,38 @@
+package com.example.mankomania;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivityLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button login=findViewById(R.id.Login_LoginButton);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent=new Intent(MainActivityLogin.this, GameScore.class);
+                startActivity(loginIntent);
+            }
+        });
+
+    }
+}
