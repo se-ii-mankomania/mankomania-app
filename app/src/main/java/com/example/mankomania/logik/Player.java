7cc47511eb40182147d6cfa159c2b195229088fd
Mathias Spionjak <mathias@Mathiass-MacBook-Pro.local>
2024-04-05 13:31:56 +0200

    Klammern vergessen

--- app/src/main/java/com/example/mankomania/logik/Player.java
+++ app/src/main/java/com/example/mankomania/logik/Player.java
@@ -11,7 +11,7 @@ public class Player {
     private Wallet wallet;
     private Map<String, Integer> stocks ;
     private int position;
-    private SecureRandom random = new SecureRandom;
+    private SecureRandom random = new SecureRandom();
 
     public Player(String username, Color color) {
         this.id = random.nextInt(10000);
