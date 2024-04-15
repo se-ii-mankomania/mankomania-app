b0aaa8d81d9fc265f945314fc3dc9ebd1ea209d9
Katrin Herold <KatrinHerold98@live.com>
2024-04-15 20:19:58 +0200

    public modifier aus HttpClientTests.java entfernt

--- app/src/test/java/com/example/mankomania/apitests/HttpClientTests.java
+++ app/src/test/java/com/example/mankomania/apitests/HttpClientTests.java
@@ -11,23 +11,23 @@ import org.junit.jupiter.api.Test;
 public class HttpClientTests {
 
     @Test
-    public void testGetHttpClient() {
+    void testGetHttpClient() {
         OkHttpClient httpClient = HttpClient.getHttpClient();
         assertEquals(OkHttpClient.class, httpClient.getClass());
     }
 
     @Test
-    public void testGetServer() {
+    void testGetServer() {
         assertEquals("http://10.0.2.2", HttpClient.getServer());
     }
 
     @Test
-    public void testGetPort() {
+    void testGetPort() {
         assertEquals(3000, HttpClient.getPort());
     }
 
     @Test
-    public void testGetHttpClientSingleton() {
+    void testGetHttpClientSingleton() {
         OkHttpClient httpClient1 = HttpClient.getHttpClient();
         OkHttpClient httpClient2 = HttpClient.getHttpClient();
         assertEquals(httpClient1, httpClient2);
