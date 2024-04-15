d8fa448dd6dc85b734cb5baee3b2c0d8051b0b63
laulaobm <131969052+laulaobm@users.noreply.github.com>
2024-04-15 15:15:11 +0200

    removed Test in StockTypesTest

--- app/src/test/java/com/example/mankomania/LogikTest/StockTypesTest.java
+++ app/src/test/java/com/example/mankomania/LogikTest/StockTypesTest.java
@@ -1,14 +1,11 @@
 package com.example.mankomania.LogikTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.example.mankomania.logik.StockTypes;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.Optional;
 
 public class StockTypesTest {
     //Testet ob die richtigen Namen zurückgegeben werden
@@ -23,12 +20,5 @@ public class StockTypesTest {
     @Test
     public void testNameOfKURZSCHLUSS_VERSORGUNGS_AG(){
         assertEquals("Kurzschluss-Versorgungs AG", StockTypes.KURZSCHLUSS_VERSORGUNGS_AG.getName());
-    }
-
-    //Stellt sicher, dass alle Aktien vorhanden sind
-    @Test
-    public void testNumberOfEnumValues(){
-        StockTypes[] values=StockTypes.values();
-        assertEquals(3, Arrays.stream(values).count());
     }
 }
