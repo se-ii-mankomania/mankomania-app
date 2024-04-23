860960e749e2de1d775ba0f83c0e64948260d8c4
Evap <evatheresapi@edu.aau.at>
2024-04-23 15:00:23 +0200

    made x and y in Cellposition private and implemented getters

--- app/src/main/java/com/example/mankomania/screens/Cellposition.java
+++ app/src/main/java/com/example/mankomania/screens/Cellposition.java
@@ -1,11 +1,18 @@
 package com.example.mankomania.screens;
 
 public class Cellposition {
-    public int x;
-    public int y;
+    private int x;
+    private int y;
 
     public Cellposition(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
     }
 }
