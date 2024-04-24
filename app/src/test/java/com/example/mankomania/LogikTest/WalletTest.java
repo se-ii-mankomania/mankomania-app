4368ffde27389ee3c80aa5812780e239571ab3f9
Mathias Spionjak <mathias@Mathiass-MacBook-Pro.local>
2024-04-24 11:21:38 +0200

    Issue änderung

--- app/src/test/java/com/example/mankomania/LogikTest/WalletTest.java
+++ app/src/test/java/com/example/mankomania/LogikTest/WalletTest.java
@@ -17,7 +17,7 @@ public class WalletTest {
 
     //Stellt sicher, dass das hinzufügen funktioniert und der Gesamtbetrag aktualisiert wird
     @Test
-    public void testAddMoneyIncreasesTotalAmount() {
+    void testAddMoneyIncreasesTotalAmount() {
         int initialTotal = wallet.totalAmount();
         wallet.addMoney(NoteTypes.FIVETHOUSAND, 2);
         int addedValue = 2 * NoteTypes.FIVETHOUSAND.getValue();
@@ -25,7 +25,7 @@ public class WalletTest {
     }
 
     @Test
-    public void testRemoveMoneyDecreasesTotalAmount(){
+    void testRemoveMoneyDecreasesTotalAmount(){
         int initialTotal = wallet.totalAmount();
         wallet.removeMoney(NoteTypes.TENTHOUSAND, 2);
         int removedValue = 2 * NoteTypes.TENTHOUSAND.getValue();
@@ -33,7 +33,7 @@ public class WalletTest {
     }
 
     @Test
-    public void testRemoveMoneyWithNegativeAmount() {
+    void testRemoveMoneyWithNegativeAmount() {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
                 wallet.removeMoney(NoteTypes.FIVETHOUSAND, -1)
         );
@@ -41,20 +41,20 @@ public class WalletTest {
     }
 
     @Test
-    public void testRemoveMoneyWithZeroAmount(){
+    void testRemoveMoneyWithZeroAmount(){
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> wallet.removeMoney(NoteTypes.FIVETHOUSAND, 0));
         assertEquals("Der zu entfernende Betrag muss größer als 0 sein.", exception.getMessage());
     }
 
     @Test
-    public void testRemoveMoneyWithSufficientNotes(){
+    void testRemoveMoneyWithSufficientNotes(){
         wallet.removeMoney(NoteTypes.HUNDREDTHOUSAND, 2);
         assertEquals(4, wallet.getNoteCount(NoteTypes.HUNDREDTHOUSAND));
     }
     //Prüft ob die removeMoney Methode funktioniert auch wenn man nicht genug Scheine von einem Typ hat jedoch genug Geld in der Börse
     //um diesen Betrag zu begleichen. Es wird der nächst kleinere Scheintyp hierführ herangezogen
     @Test
-    public void testRemoveMoneyWithInsufficientNotesButSufficientTotal() {
+    void testRemoveMoneyWithInsufficientNotesButSufficientTotal() {
         wallet.removeMoney(NoteTypes.HUNDREDTHOUSAND, 7);
         assertEquals(300000, wallet.totalAmount());
         assertEquals(0, wallet.getNoteCount(NoteTypes.HUNDREDTHOUSAND));
@@ -62,7 +62,7 @@ public class WalletTest {
     }
 
     @Test
-    public void testRemoveMoneyWithInsufficientTotal(){
+    void testRemoveMoneyWithInsufficientTotal(){
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, ()->
                 wallet.removeMoney(NoteTypes.HUNDREDTHOUSAND, 11));
         assertEquals("Sie haben gewonnen!", exception.getMessage());
@@ -70,13 +70,13 @@ public class WalletTest {
 
 
     @Test
-    public void testTotalAmountCalculatesCorrectly() {
+    void testTotalAmountCalculatesCorrectly() {
         int expectedTotal = (6 * 5000) + (7 * 10000) + (6 * 50000) + (6 * 100000);
         assertEquals(expectedTotal, wallet.totalAmount());
     }
 
     @Test
-    public void testCheatMoneyDecreasesTotalAmount() throws Exception {
+    void testCheatMoneyDecreasesTotalAmount() throws Exception {
         int initialTotal = wallet.totalAmount();
         wallet.cheatMoney(NoteTypes.HUNDREDTHOUSAND, 1);
         int cheatedValue = NoteTypes.HUNDREDTHOUSAND.getValue();
