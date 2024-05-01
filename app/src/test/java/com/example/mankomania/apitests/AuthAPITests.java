5633eacc4b8c8cdb53c22a6d8e89077d7b767510
Katrin Herold <KatrinHerold98@live.com>
2024-05-01 13:02:08 +0200

    AuthAPITests für createRequest 1 Test

--- app/src/test/java/com/example/mankomania/apitests/AuthAPITests.java
+++ app/src/test/java/com/example/mankomania/apitests/AuthAPITests.java
@@ -4,6 +4,8 @@ import com.example.mankomania.api.AuthAPI;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import okhttp3.*;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
@@ -59,5 +61,22 @@ public class AuthAPITests {
 
         assertFalse(result.has("email"));
         assertFalse(result.has("password"));
+    }
+
+    @Test
+    void testCreateRequest() throws JSONException {
+        JSONObject jsonRequest = new JSONObject();
+        jsonRequest.put("email", "test@test.at");
+        jsonRequest.put("password", "my_password");
+        String path = "/some/path";
+
+        Request request = AuthAPI.createRequest(jsonRequest, path);
+
+        // creating a Request expectedRequest and calling assertEquals(expectedRequest, request);
+        // does not work here (AssertionFailedError since the references differ)
+
+        assertEquals("http://10.0.2.2:3000/some/path", request.url().toString());
+        assertEquals("POST", request.method());
+        assertNotNull(request.body());
     }
 }
