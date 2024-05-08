1de60ea09edd6492e6928b05d8743c6f82dfb1c8
Katrin Herold <KatrinHerold98@live.com>
2024-05-08 22:05:52 +0200

    added tests for createJSONObject(..)

--- app/src/test/java/com/example/mankomania/apitests/SessionAPITests.java
+++ app/src/test/java/com/example/mankomania/apitests/SessionAPITests.java
@@ -1,10 +1,13 @@
 package com.example.mankomania.apitests;
 
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.example.mankomania.logik.spieler.Color;
 import com.example.mankomania.api.SessionAPI;
+
+import java.util.UUID;
 
 public class SessionAPITests {
     @Test
@@ -15,5 +18,29 @@ public class SessionAPITests {
         assertEquals(Color.PURPLE, SessionAPI.convertToEnums("lila"));
 
         assertNull(SessionAPI.convertToEnums("invalid_color"));
+    }
+
+    @Test
+    void testCreateJSONObject_UUID() {
+        // prepare input
+        UUID lobbyid = UUID.randomUUID();
+
+        // call method
+        JSONObject jsonObject = SessionAPI.createJSONObject(lobbyid);
+
+        // assert
+        assertTrue(jsonObject.has("lobbyid"));
+    }
+
+    @Test
+    void testCreateJSONObject_Color() {
+        // prepare input
+        String color = "Rot";
+
+        // call method
+        JSONObject jsonObject = SessionAPI.createJSONObject(color);
+
+        // assert
+        assertTrue(jsonObject.has("color"));
     }
 }
