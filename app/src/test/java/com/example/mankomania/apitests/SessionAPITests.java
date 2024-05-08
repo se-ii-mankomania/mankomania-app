3e9246d1463f311171d550c0b22e4577a3f182ef
Katrin Herold <KatrinHerold98@live.com>
2024-05-08 22:17:27 +0200

    added tests for create[Get/Post]Request(..)

--- app/src/test/java/com/example/mankomania/apitests/SessionAPITests.java
+++ app/src/test/java/com/example/mankomania/apitests/SessionAPITests.java
@@ -1,5 +1,6 @@
 package com.example.mankomania.apitests;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,6 +9,8 @@ import com.example.mankomania.logik.spieler.Color;
 import com.example.mankomania.api.SessionAPI;
 
 import java.util.UUID;
+
+import okhttp3.Request;
 
 public class SessionAPITests {
     @Test
@@ -42,5 +45,34 @@ public class SessionAPITests {
 
         // assert
         assertTrue(jsonObject.has("color"));
+    }
+
+    @Test
+    void testCreateGetRequest() {
+        // set up
+        String token = "test_token";
+        String path = "/api/session/unavailableColors/1234";
+
+        // call method
+        Request request = SessionAPI.createGetRequest(token, path);
+
+        // assert
+        assertEquals("http://10.0.2.2:3000/api/session/unavailableColors/1234", request.url().toString());
+    }
+
+    @Test
+    void testCreatePostRequest() throws JSONException {
+        // set up
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("color", "Rot");
+
+        String token = "test_token";
+        String path = "/api/session/setColor/1234";
+
+        // call method
+        Request request = SessionAPI.createPostRequest(jsonObject, token, path);
+
+        // assert
+        assertEquals("http://10.0.2.2:3000/api/session/setColor/1234", request.url().toString());
     }
 }
