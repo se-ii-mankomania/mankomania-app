45e7120a3ca0317c03f4a47fb79e8bcc7651a926
laulaobm <131969052+laulaobm@users.noreply.github.com>
2024-06-06 00:56:15 +0200

    merged if statements with enclosing ones according to Sonar

--- app/src/main/java/com/example/mankomania/screens/SwipeGestureListener.java
+++ app/src/main/java/com/example/mankomania/screens/SwipeGestureListener.java
@@ -19,12 +19,8 @@ public class SwipeGestureListener extends GestureDetector.SimpleOnGestureListene
     public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
         float diffY = e2.getY() - Objects.requireNonNull(e1).getY();
         float diffX = e2.getX() - e1.getX();
-        if (Math.abs(diffX) < Math.abs(diffY)) {
-            if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
-                if (diffY < 0) {
-                    onSwipeUp();
-                }
-            }
+        if (Math.abs(diffX) < Math.abs(diffY) && Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD && diffY<0) {
+            onSwipeUp();
         }
         return true;
     }
