b3fdbaeb2f9937c96ac43cb49666a62f1f4d6f14
kevinunigithub <kevingo@edu.aau.at>
2024-06-12 19:22:26 +0200

    initial activity

---
+++ app/src/main/java/com/example/mankomania/screens/Boese1.java
@@ -0,0 +1,180 @@
+package com.example.mankomania.screens;
+
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.activity.OnBackPressedCallback;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.mankomania.R;
+import com.example.mankomania.logik.spieler.Dice;
+
+public class Boese1 extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private boolean backPressedBlocked;
+    private int sum = 0;
+    private boolean rolling = true;
+    private TextView resultSumTextView;
+
+    private static final int SENSIBILITY_BORDER_FOR_SENSOR = 10;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        blockBackButton();
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_boese1);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        ToolbarFunctionalities.setUpToolbar(this);
+
+        OnBackPressedCallback callback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                if (backPressedBlocked) {
+                    Toast.makeText(Boese1.this, "Bitte würfeln!", Toast.LENGTH_SHORT).show();
+                } else {
+                    this.setEnabled(false);
+                    Boese1.super.finish();
+                }
+            }
+        };
+        getOnBackPressedDispatcher().addCallback(this, callback);
+
+        resultSumTextView = findViewById(R.id.RollDice_Sum);
+
+        Button rollDiceButton = findViewById(R.id.RollDice_RollingDiceButton);
+        rollDiceButton.setOnClickListener(v -> rollDice());
+
+        Button stopRollingButton = findViewById(R.id.StopRollingButton);
+        stopRollingButton.setOnClickListener(v -> stopRolling());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (rolling && event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+            if ((Math.abs(x) > SENSIBILITY_BORDER_FOR_SENSOR || Math.abs(y) > SENSIBILITY_BORDER_FOR_SENSOR || Math.abs(z) > SENSIBILITY_BORDER_FOR_SENSOR)) {
+                rollDice();
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // No need to handle accuracy changes for this implementation
+    }
+
+    private void blockBackButton() {
+        this.backPressedBlocked = true;
+    }
+
+    private void unblockBackButton() {
+        this.backPressedBlocked = false;
+    }
+
+    private void rollDice() {
+        Dice dice = new Dice();
+        int[] randomNumber = dice.throwDice();
+
+        if (randomNumber[0] == 1 || randomNumber[1] == 1) {
+            rolling = false;
+            Toast.makeText(getApplicationContext(), "Du hast eine 1 gewürfelt!", Toast.LENGTH_SHORT).show();
+            //TODO Api Call here
+            unblockBackButton();
+        }
+
+        sum += randomNumber[0] + randomNumber[1];
+        ImageView diceOne = findViewById(R.id.RollDice_diceOne);
+        ImageView diceTwo = findViewById(R.id.RollDice_diceTwo);
+
+        int sourceDiceOne = getDiceDrawable(randomNumber[0]);
+        int sourceDiceTwo = getDiceDrawable(randomNumber[1]);
+
+        diceOne.setImageResource(sourceDiceOne);
+        diceTwo.setImageResource(sourceDiceTwo);
+
+        resultSumTextView.setText("Dein Ergebnis lautet: " + sum);
+
+        if (!rolling) {
+            Button rollDiceButton = findViewById(R.id.RollDice_RollingDiceButton);
+            rollDiceButton.setEnabled(false);
+            navigateBackToBoard();
+        }
+    }
+
+
+    private void stopRolling() {
+        rolling = false;
+        Toast.makeText(getApplicationContext(), "Du hast ein Summe von " + sum +" gewürfelt.", Toast.LENGTH_SHORT).show();
+        //TODO API Call here
+        Button rollDiceButton = findViewById(R.id.RollDice_RollingDiceButton);
+        rollDiceButton.setEnabled(false);
+        unblockBackButton();
+        navigateBackToBoard();
+    }
+
+    private int getDiceDrawable(int result) {
+        switch (result) {
+            case 1:
+                return R.drawable.dice1;
+            case 2:
+                return R.drawable.dice2;
+            case 3:
+                return R.drawable.dice3;
+            case 4:
+                return R.drawable.dice4;
+            case 5:
+                return R.drawable.dice5;
+            case 6:
+                return R.drawable.dice6;
+            default:
+                return R.drawable.dice;
+        }
+    }
+    private void navigateBackToBoard(){
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Intent backToBoard = new Intent(Boese1.this, Board.class);
+            startActivity(backToBoard);
+            unblockBackButton();
+        }, 2000);
+    }
+}
