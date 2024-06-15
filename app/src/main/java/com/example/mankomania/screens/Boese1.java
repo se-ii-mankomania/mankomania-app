e99a784add8e7f508bb350d542b7ccb4cb149def
kevinunigithub <kevingo@edu.aau.at>
2024-06-15 19:29:49 +0200

    added API Call

--- app/src/main/java/com/example/mankomania/screens/Boese1.java
+++ app/src/main/java/com/example/mankomania/screens/Boese1.java
@@ -2,6 +2,7 @@ package com.example.mankomania.screens;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -20,9 +21,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.security.crypto.EncryptedSharedPreferences;
+import androidx.security.crypto.MasterKey;
 
 import com.example.mankomania.R;
 import com.example.mankomania.logik.spieler.Dice;
+import com.example.mankomania.api.Boese1API;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.UUID;
 
 public class Boese1 extends AppCompatActivity implements SensorEventListener {
 
@@ -30,7 +38,13 @@ public class Boese1 extends AppCompatActivity implements SensorEventListener {
     private Sensor accelerometer;
     private boolean backPressedBlocked;
     private int sum = 0;
+
+    private int oneCounter = 0;
     private boolean rolling = true;
+
+    private String token;
+    private UUID lobbyid;
+    private UUID userId;
     private TextView resultSumTextView;
 
     private static final int SENSIBILITY_BORDER_FOR_SENSOR = 10;
@@ -46,6 +60,8 @@ public class Boese1 extends AppCompatActivity implements SensorEventListener {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        initSharedPreferences();
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
@@ -116,9 +132,15 @@ public class Boese1 extends AppCompatActivity implements SensorEventListener {
         int[] randomNumber = dice.throwDice();
 
         if (randomNumber[0] == 1 || randomNumber[1] == 1) {
+            if (randomNumber[0] == 1) {
+                oneCounter++;
+            }
+            if (randomNumber[1] == 1) {
+                oneCounter++;
+            }
             rolling = false;
             Toast.makeText(getApplicationContext(), "Du hast eine 1 gewürfelt!", Toast.LENGTH_SHORT).show();
-            //TODO Api Call here
+            sendApiRequest(token, lobbyid, sum, oneCounter);
             unblockBackButton();
         }
 
@@ -145,7 +167,7 @@ public class Boese1 extends AppCompatActivity implements SensorEventListener {
     private void stopRolling() {
         rolling = false;
         Toast.makeText(getApplicationContext(), "Du hast ein Summe von " + sum +" gewürfelt.", Toast.LENGTH_SHORT).show();
-        //TODO API Call here
+        sendApiRequest(token, lobbyid, sum, oneCounter);
         Button rollDiceButton = findViewById(R.id.RollDice_RollingDiceButton);
         rollDiceButton.setEnabled(false);
         unblockBackButton();
@@ -176,5 +198,44 @@ public class Boese1 extends AppCompatActivity implements SensorEventListener {
             startActivity(backToBoard);
             unblockBackButton();
         }, 2000);
+    }
+
+    private void initSharedPreferences() {
+        try {
+            MasterKey masterKey = new MasterKey.Builder(this)
+                    .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+                    .build();
+
+            SharedPreferences sharedPreferences = EncryptedSharedPreferences.create(
+                    this,
+                    "MyPrefs",
+                    masterKey,
+                    EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                    EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+            );
+
+            token = sharedPreferences.getString("token", null);
+            String lobbyidString = sharedPreferences.getString("lobbyid", null);
+            lobbyid=UUID.fromString(lobbyidString);
+            String useridString = sharedPreferences.getString("userId", null);
+            userId=UUID.fromString(useridString);
+        } catch (GeneralSecurityException | IOException ignored) {
+            Toast.makeText(getApplicationContext(), "SharedPreferences konnten nicht geladen werden.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void sendApiRequest(String token, UUID lobbyid, int sum, int one){
+        Thread thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+                    Boese1API.sendBoeseRequest(token, lobbyid, sum, one);
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "Änderungen konnten nicht gespeichert werden.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        thread.start();
     }
 }
